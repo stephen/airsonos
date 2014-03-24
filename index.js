9ef7aefe31af7752aaaf3e9818fb0c58bf0d6f59
Stephen Wan <stephen@stephenwan.net>
2014-03-23 19:41:19 -0500

    setup generalized search/discovery

--- index.js
+++ index.js
@@ -1,24 +1,48 @@
 "use strict";
 var sonos = require('sonos');
+var portastic = require('portastic');
 var ip = require('ip');
 var NodeTunes = require('nodetunes');
 var NicerCast = require('nicercast');
 
-var audioStream = require('stream').PassThrough();
 
-var airplayServer = new NodeTunes(audioStream);
-var icecastServer = new NicerCast(audioStream);
-var outputPort = 8001;
+sonos.search(function(device) {
+  console.log('setting up airsonos for', device);
 
-airplayServer.start();
-icecastServer.start(outputPort);
+  var audioStream = require('stream').PassThrough();
 
-airplayServer.on('clientConnected', function() {
-	var device = new sonos.Sonos('172.17.105.103');
+  var airplayServer = new NodeTunes(audioStream, {
+    serverName: 'AirSonos@' + device.host
+  });
+  var icecastServer = new NicerCast(audioStream, {
+    name: 'AirSonos@' + device.host
+  });
 
-	// device is an instance of sonos.Sonos
-	device.play('x-rincon-mp3radio://' + ip.address() + ':' + outputPort + '/listen.m3u', function(err, playing) {
-		device.play(function() {
-		});
-	});
+  portastic.find({
+      min : 8000,
+      max : 8050,
+      retrieve: 1
+  }, function(err, port) {
+    if (err) throw err;
+
+    airplayServer.on('clientConnected', function() {
+
+      // device is an instance of sonos.Sonos
+      device.play('x-rincon-mp3radio://' + ip.address() + ':' + port + '/listen.m3u', function(err, playing) {
+        device.play(function() {
+          
+        });
+      });
+    });
+
+    airplayServer.on('volumeChange', function(vol) {
+      vol = 100 - Math.floor(-1 * (Math.max(vol, -30) / 30) * 100);
+      device.setVolume(vol, function() {
+
+      });
+    });
+
+    airplayServer.start();
+    icecastServer.start(port);
+  });
 });
